ebec3f598c5ad12db5952a63ee21e33f1833a8b2
jnpooh0329 <105191407+jnpooh0329@users.noreply.github.com>
2022-09-01 13:33:22 +0900

    프로젝트 업데이트

---
+++ Web-study-11/src/com/saeyan/dao/BoardDAO.java
@@ -0,0 +1,16 @@
+package com.saeyan.dao;
+
+public class BoardDAO {
+	private BoardDAO() {
+		
+	}
+	
+	private static BoardDAO instance = new BoardDAO();
+	
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	
+	
+	
+}
